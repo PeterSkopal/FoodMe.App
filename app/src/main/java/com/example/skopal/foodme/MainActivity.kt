e283dc0cd308404399f0bccff3292d17625adfd9
Peter Skopal <peter.skopal@hotmail.com>
2018-10-05 08:52:17 +0200

    feat(navigation): add retriction on bottom navigation bar, not shifting icons while navigating.

--- app/src/main/java/com/example/skopal/foodme/MainActivity.kt
+++ app/src/main/java/com/example/skopal/foodme/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.skopal.foodme
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -37,6 +42,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigation.disableShiftMode()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    /**
+     * disable shift mode on Android BottomNavigationView
+     * https://stackoverflow.com/questions/40176244/how-to-disable-bottomnavigationview-shift-mode
+     */
+    @SuppressLint("RestrictedApi")
+    fun BottomNavigationView.disableShiftMode() {
+        val menuView = getChildAt(0) as BottomNavigationMenuView
+        try {
+            val shiftingMode = menuView::class.java.getDeclaredField("mShiftingMode")
+            shiftingMode.isAccessible = true
+            shiftingMode.setBoolean(menuView, false)
+            shiftingMode.isAccessible = false
+            for (i in 0 until menuView.childCount) {
+                val item = menuView.getChildAt(i) as BottomNavigationItemView
+                item.setShiftingMode(false)
+                // set once again checked value, so view will be updated
+                item.setChecked(item.itemData.isChecked)
+            }
+        } catch (e: NoSuchFieldException) {
+            Log.e(ContentValues.TAG, "Unable to get shift mode field", e)
+        } catch (e: IllegalStateException) {
+            Log.e(ContentValues.TAG, "Unable to change value of shift mode", e)
+        }
     }
 }
