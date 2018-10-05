554184a270f982a75535dda4328651a7b1fc3829
Peter Skopal <peter.skopal@hotmail.com>
2018-10-05 13:10:34 +0200

    feat(frag-nav): navigate amongst fragments when utilizing the bottom navigation bar.

---
+++ app/src/main/java/com/example/skopal/foodme/ActivityUtil.kt
@@ -0,0 +1,10 @@
+package com.example.skopal.foodme
+
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
