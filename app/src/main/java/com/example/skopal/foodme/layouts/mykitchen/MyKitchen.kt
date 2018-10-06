25e0af5e3c0832c9db26a6ca837de31e125fadca
Peter Skopal <peter.skopal@hotmail.com>
2018-10-06 13:05:37 +0200

    feat(title): add title handling from fragments, allowing fragments to change title in application headline.

--- app/src/main/java/com/example/skopal/foodme/layouts/mykitchen/MyKitchen.kt
+++ app/src/main/java/com/example/skopal/foodme/layouts/mykitchen/MyKitchen.kt
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.skopal.foodme.MainActivity
 import com.example.skopal.foodme.R
 
 class MyKitchen : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        (activity as MainActivity).setActionBarTitle(getString(R.string.title_kitchen))
         return inflater.inflate(R.layout.fragment_my_kitchen, container, false)
     }
 
