86c95abebe25693442487d7e7eff910a5c23a0c0
Peter Skopal <peter.skopal@hotmail.com>
2018-10-10 13:10:04 +0200

    feat(api): hook up Food-Me-Api ping endpoint. Is viewable in Scanner view.

--- app/src/main/java/com/example/skopal/foodme/layouts/scanner/Scanner.kt
+++ app/src/main/java/com/example/skopal/foodme/layouts/scanner/Scanner.kt
@@ -6,16 +6,32 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.skopal.foodme.MainActivity
 import com.example.skopal.foodme.R
+import com.example.skopal.foodme.services.FoodMeApiPing
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.launch
 
 class Scanner : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        var view = inflater.inflate(R.layout.fragment_scanner, container, false)
+
         (activity as MainActivity).setActionBarTitle(getString(R.string.title_scanner))
-        return inflater.inflate(R.layout.fragment_scanner, container, false)
+
+        val text = view.findViewById<TextView>(R.id.scanner_text)
+
+        FoodMeApiPing().getPing { res ->
+            GlobalScope.launch(Dispatchers.Main) {
+                text.text = "${text.text}: $res"
+            }
+        }
+
+        return view
     }
     
 }
---
+++ app/src/main/java/com/example/skopal/foodme/services/FoodMeApiPing.kt
@@ -0,0 +1,14 @@
+package com.example.skopal.foodme.services
+
+import com.example.skopal.foodme.constants.FoodMeApiConstants
+
+class FoodMeApiPing {
+    private val baseUrl = "${FoodMeApiConstants.BASE_URL}${FoodMeApiConstants.PING}"
+
+    fun getPing(cb: (String) -> Unit) {
+        khttp.async.get(baseUrl) {
+            cb(text)
+        }
+    }
+
+}
